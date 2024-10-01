2db3408b66c8f1c08633ec6fcbaa26156258ec9c
sgtudeveloper <webdeveloper.igcsm@gmail.com>
2024-10-01 14:33:41 +0530

    added seo

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,12 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "Igcsm Franchisee",
-  description: "India's No.1 Franchisee Network",
+  title: "Computer Franchisee Network || IGCSM",
+  description:
+    "is a not-for-profit organization dedicated to empowering underprivileged communities    across India through education, skill development, and microfinance initiatives. Established in 2008 and headquartered in Delhi, IGCSM operates in over 21 states, impacting more than 500,000 people, including children, youth, and women. With a network of 500+",
+  robots: "index, follow",
+  viewport: "width=device-width, initial-scale=1",
+  charset: "utf-8",
 };
 
 export default function RootLayout({ children }) {
